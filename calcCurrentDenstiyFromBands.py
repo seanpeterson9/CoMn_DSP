dc0565fee060390b7970ca1b2e816db3a045f89a
seanpeterson9 <seanpeterson9@montana.edu>
2026-06-30 01:33:47 -0600

    The code now takes in reorganized data files that can be read without a QATK license, if you lose internet access for 30 minutes then QATK fails heartbeat checks that cause it to terminate your code and ruin your day.

--- calcCurrentDenstiyFromBands.py
+++ calcCurrentDenstiyFromBands.py
@@ -48,21 +48,7 @@ def vk(interp_func,kx,ky,kz,dk = 1e-6):
     return vx, vy, vz
 
 def genInterpFuncs(kpoints,kx,ky,kz,E,nbands,a,b,c,E_cutoff,N_k):
-    """
-    Kx = np.concatenate((kx,ky,-kx,-ky,kx,ky,-kx,-ky,kx,ky,-kx,-ky,kx,ky,-kx,-ky))
-    Ky = np.concatenate((ky,kx,ky,kx,-ky,-kx,-ky,-kx,ky,kx,ky,kx,-ky,-kx,-ky,-kx))
-    Kz = np.concatenate((kz,kz,kz,kz,kz,kz,kz,kz,-kz,-kz,-kz,-kz,-kz,-kz,-kz,-kz))
-    mask = (Kx > -.05) & (Ky > -.05) & (Kz > -.05)
-    Kx = Kx[mask]
-    Ky = Ky[mask]
-    Kz = Kz[mask]
-    E_k = []
-    for i in range(0,2*nbands):
-        E_k.append(np.concatenate((E[i],E[i],E[i],E[i],E[i],E[i],E[i],E[i],E[i],E[i],E[i],E[i],E[i],E[i],E[i],E[i]))[mask])
-    E_k = np.array(E_k)
     
-    sort = np.argsort(Kx)
-    """
     E = np.array(E)
     bandID = []
     interp_band_list = []
@@ -90,22 +76,36 @@ def genInterpFuncs(kpoints,kx,ky,kz,E,nbands,a,b,c,E_cutoff,N_k):
     
     return interp_band_list,bandID
 
-def readBandFile(Filename):
-    bandStruct = nlread(Filename)[-1]
-    bands = bandStruct.evaluate().inUnitsOf(eV)
-    
-    kx = np.array(bandStruct.kpoints()[:,0])
-    ky = np.array(bandStruct.kpoints()[:,1])
-    kz = np.array(bandStruct.kpoints()[:,2])
-    E = []
-    nbands = len(bands[0][0])
-    for i in range(0,2*nbands):
-        if i < nbands:
-            E.append(bands[0][:,i])
-        else:
-            E.append(bands[1][:,i-nbands])
-    E = np.array(E)
-    return np.array(bandStruct.kpoints()),kx,ky,kz,E,nbands
+def readDatFile(Filename):
+    with open(Filename) as f:
+        F = csv.reader(f,delimiter='\t')
+        j = -1
+        for row in F:
+            
+            if j == -1:
+                N_k = int(row[0])
+                nbands = int(row[1])
+                a = float(row[2])
+                c = float(row[3])
+                
+                kx = np.zeros(N_k)
+                ky = np.zeros(N_k)
+                kz = np.zeros(N_k)
+                
+                E = np.zeros((2*nbands,N_k))
+            
+            else:
+                kx[j] = float(row[0])
+                ky[j] = float(row[1])
+                kz[j] = float(row[2])
+                for i in range(0,2*nbands):
+                    E[i][j] = float(row[i+3])
+            j = j + 1
+    kpoints = []
+    for i in range(0,len(kx)):
+        kpoints.append([kx[i],ky[i],kz[i]])
+    kpoints = np.array(kpoints)
+    return kpoints,kx,ky,kz,E,nbands,a,c
 
 def genEquipotentialFsolve(interp_band_func,E,dk,dkz_guess):
     
@@ -173,26 +173,14 @@ def genEquipotentialFsolve(interp_band_func,E,dk,dkz_guess):
     kx = np.array(kx_1d)
     ky = np.array(ky_1d)
     kz = np.array(kz_1d)
-    """
-    kx_midpoint = kx + dk/2
-    ky_midpoint = ky + dk/2
-    kz_midpoint = np.zeros(len(kx))
-
-    for i in range(0,len(kx)):
-        kz_midpoint[i] = .5/(1+np.exp(-.01*fsolve(lambda k: (E-interp_band_func((kx_midpoint[i],ky_midpoint[i],.5/(1+np.exp(-.01*k)))))**2,x0=[100*np.log(2*kz[i]/(1-2*kz[i]))],factor=0.1)))
-    """
+    
     vx,vy,vz = vk(interp_band_func,kx,ky,kz)
     dSk = dk*dk*np.sqrt(1. + (vx/vz)**2 + (vy/vz)**2)
     
-    #vx,vy,vz = vk(interp_band_func,kx_midpoint,ky_midpoint,kz_midpoint)
     v = np.sqrt(vx**2 + vy**2 + vz**2)
     
-    #mask = (kz_midpoint >= 0) & (kz_midpoint <= .5) & (kx_midpoint <= ky_midpoint) & (~np.isnan(v)) & (np.abs(v) > 1e-5) & ((E-interp_band_func(np.column_stack([kx_midpoint,ky_midpoint,kz_midpoint])))**2 < 1e-5) & (dSk < 10*dk*dk)
     mask = (kz >= 0) & (kz <= .5) & (~np.isnan(v)) & (np.abs(v) > 1e-5) & ((E-interp_band_func(np.column_stack([kx,ky,kz])))**2 < 1e-5) & (dSk < 10*dk*dk)
     
-    #kx_midpoint_symm = np.concatenate((kx_midpoint[mask],ky_midpoint[mask]))
-    #ky_midpoint_symm = np.concatenate((ky_midpoint[mask],kx_midpoint[mask]))
-    #kz_midpoint_symm = np.concatenate((kz_midpoint[mask],kz_midpoint[mask]))
     kx_symm = np.concatenate((kx[mask],ky[mask]))
     ky_symm = np.concatenate((ky[mask],kx[mask]))
     kz_symm = np.concatenate((kz[mask],kz[mask]))
@@ -202,8 +190,6 @@ def genEquipotentialFsolve(interp_band_func,E,dk,dkz_guess):
     v_symm = np.concatenate((v[mask],v[mask]))
     dSk_symm = np.concatenate((dSk[mask],dSk[mask]))
     
-    #return kx_midpoint_symm,ky_midpoint_symm,kz_midpoint_symm,dSk_symm,vz_symm,v_symm
-    #return kx_symm,ky_symm,kz_symm,dSk_symm,vz_symm,v_symm
     return kx[mask],ky[mask],kz[mask],dSk[mask],vz[mask],v[mask]
 
 def genEquipotentialGrid(interp_band_func_list,E_cutoff,N_E,N_theta,N_phi):
@@ -230,7 +216,7 @@ def genEquipotentialGrid(interp_band_func_list,E_cutoff,N_E,N_theta,N_phi):
             dS[i].append([])
             vz[i].append([])
             v[i].append([])
-            #kx_tmp,ky_tmp,kz_tmp,dS_tmp,vz_tmp,v_tmp = genEquipotentialFsolve(interp_band_func_list[i],E_grid[j],.015625,.125)
+            
             kx_tmp,ky_tmp,kz_tmp,dS_tmp,vz_tmp,v_tmp = genEquipotentialFsolve(interp_band_func_list[i],E_grid[j],.01,.125)
             print(i,E_grid[j],len(kx_tmp))
             kx[i][j] = kx_tmp
@@ -638,22 +624,6 @@ def integrateBands(E_k,dS_k,vz_k,v_k,bandID,nbands,T):
     dE = np.abs(E_k[1] - E_k[0])
     
     tau = []
-    """
-    print('calculating better electron lifetimes')
-    for i in range(0,len(bandID)):
-        tau.append([])
-        for n in range(0,len(E_k)):
-            tau[i].append([])
-            tau_inv_tmp = 0.
-            for j in range(0,len(bandID)):
-                if ((bandID[i] < nbands) and (bandID[j] < nbands)) or ((bandID[i] >= nbands) and (bandID[j] >= nbands)):
-                    for m in range(0,len(E_k)):
-                        tau_inv_tmp = tau_inv_tmp + dE*np.sum(dS_k[j][m][v_k[j][m] != 0.]*deltaGaussHerm(E_k[m],E_k[n],.02*E_k[-1])/v_k[j][m][v_k[j][m] != 0.])
-            if tau_inv_tmp > 1e-8:
-                tau[i][n] = np.ones(len(dS_k[i][n]))/tau_inv_tmp
-            else:
-                tau[i][n] = np.zeros(len(dS_k[i][n]))
-    """
 
     print('calculating better conductivity')
     for i in range(0,len(bandID)):
@@ -740,10 +710,9 @@ def integrateBands(E_k,dS_k,vz_k,v_k,bandID,nbands,T):
     
     return NF_up,NF_down,(NF_up-NF_down)/(NF_up+NF_down),sigmaz_up0,sigmaz_down0,(sigmaz_up0-sigmaz_down0)/(sigmaz_up0+sigmaz_down0),sigmaz_up1,sigmaz_down1,(sigmaz_up1-sigmaz_down1)/(sigmaz_up1+sigmaz_down1),sigmaz_up2,sigmaz_down2,(sigmaz_up2-sigmaz_down2)/(sigmaz_up2+sigmaz_down2),sigmaz_up5,sigmaz_down5,(sigmaz_up5-sigmaz_down5)/(sigmaz_up5+sigmaz_down5),sigmaz_up7,sigmaz_down7,(sigmaz_up7-sigmaz_down7)/(sigmaz_up7+sigmaz_down7),sigmaz_up10,sigmaz_down10,(sigmaz_up10-sigmaz_down10)/(sigmaz_up10+sigmaz_down10),sigmaz_up15,sigmaz_down15,(sigmaz_up15-sigmaz_down15)/(sigmaz_up15+sigmaz_down15),sigmaz_up20,sigmaz_down20,(sigmaz_up20-sigmaz_down20)/(sigmaz_up20+sigmaz_down20),sigmaz_up30,sigmaz_down30,(sigmaz_up30-sigmaz_down30)/(sigmaz_up30+sigmaz_down30),sigmaz_up40,sigmaz_down40,(sigmaz_up40-sigmaz_down40)/(sigmaz_up40+sigmaz_down40),sigmaz_up50,sigmaz_down50,(sigmaz_up50-sigmaz_down50)/(sigmaz_up50+sigmaz_down50)
 
-def main_plotBands(Filename,a,c,E_cutoff,N_theta,N_phi,N_E,N_k = 50,checkFit = False):
-    n_fit = 5
+def main_plotBands(Filename,E_cutoff,N_theta,N_phi,N_E,N_k = 50,checkFit = False):
     print('importing bands from file')
-    kpoints,kx,ky,kz,E,nbands = readBandFile(Filename)
+    kpoints,kx,ky,kz,E,nbands,a,c = readDatFile(Filename)
     
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -753,43 +722,6 @@ def main_plotBands(Filename,a,c,E_cutoff,N_theta,N_phi,N_E,N_k = 50,checkFit = F
     print('interpolating bands')
     interp_band_list,bandID = genInterpFuncs(kpoints,kx,ky,kz,E,nbands,a,a,c,E_cutoff,int(N_k))
     
-    #fitParamsList,bandID = genFitParams(kx,ky,kz,E,kx2,ky2,kz2,E2,nbands,E_cutoff)
-    """
-    if checkFit:
-        residuals = np.zeros(len(bandID))
-        N_abinit = np.linspace(0,1,len(kx))
-        for i in range(0,len(bandID)):
-            mask = np.abs(E[bandID[i]]) < E_cutoff
-            residuals[i] = np.sum(np.abs(cosFitTest_3D(kx[mask],ky[mask],kz[mask],fitParamsList[i],n_fit)-E[bandID[i]][mask]))/len(kx[mask])
-            print(bandID[i],residuals[i])
-            if bandID[i] < nbands:
-                plt.plot(N_abinit,cosFitTest_3D(kx,ky,kz,fitParamsList[i],n_fit),c = 'blue')
-                plt.scatter(N_abinit,E[bandID[i]],c = 'blue')
-            else:
-                plt.plot(N_abinit,cosFitTest_3D(kx,ky,kz,fitParamsList[i],n_fit),c = 'red')
-                plt.scatter(N_abinit,E[bandID[i]],c = 'red')
-            plt.plot(N_abinit,np.zeros(len(N_abinit)),c = 'k',ls = ':')
-            plt.show()
-        print(np.sum(residuals)/len(bandID))
-
-    bandpath_x,bandpath_y,bandpath_z,N,labels = genBands(a,c,N_k)
-    
-    f, (a1,a2) = plt.subplots(1,2,gridspec_kw = {'width_ratios': [3,1]})
-    a1.set_xticks(labels[0],labels = labels[1])
-    a1.set_yticks([-.5,-.4,-.3,-.2,-.1,0,.1,.2,.3,.4,.5],labels = ['-0.5','-0.4','-0.3','-0.2','-0.1','0','0.1','0.2','0.3','0.4','0.5'])
-    #a1.set_yticks([-1.25,-1,-.75,-.5,.25,0,.25,.5,.75,1,1.25],labels = ['-1.25','-1','-0.75','-0.5','-0.25','0','0.25','0.5','0.75','1','1.25'])
-    a1.plot(N,np.zeros(len(N)),ls = '--',c = 'black')
-    a1.set_ylim([-.5,.5])
-    #a1.set_ylim([-1.4,1.4])
-    a1.set_xlim(N[0],N[-1])
-    for i in range(0,len(bandID)):
-        if bandID[i] < nbands:
-            #a1.plot(N,cosFit_3D_wrapper(bandpath_x,bandpath_y,bandpath_z,fitParamsList[i]),c = 'blue')
-            a1.plot(N,cosFitTest_3D(bandpath_x,bandpath_y,bandpath_z,fitParamsList[i],n_fit),c = 'blue')
-        else:
-            #a1.plot(N,cosFit_3D_wrapper(bandpath_x,bandpath_y,bandpath_z,fitParamsList[i]),c = 'red')
-            a1.plot(N,cosFitTest_3D(bandpath_x,bandpath_y,bandpath_z,fitParamsList[i],n_fit),c = 'red')
-    """
     print('generating integration grid')
     E_k,dS_k,vz_k,v_k = genEquipotentialGrid(interp_band_list,E_cutoff,N_E,N_theta,N_phi)
     E,N_up,N_down = genDOS(bandID,nbands,dS_k,v_k,E_k,E_cutoff,2*N_E)
@@ -798,24 +730,20 @@ def main_plotBands(Filename,a,c,E_cutoff,N_theta,N_phi,N_E,N_k = 50,checkFit = F
     print(' ')
     for i in range(0,len(E)):
         print(E[i],N_up[i],N_down[i])
-    
-#main_plotBands('./qatkDatFiles/atomicMag/Co_bulk_new/a_277.hdf5',2.77,2.77,.2,15,15,51)
 
-def DSP(Filename,a,c,E_cutoff,T,N_theta,N_phi,N_E,N_k = 50):
+def DSP(Filename,E_cutoff,T,N_theta,N_phi,N_E,N_k = 50):
     print('importing bands from file')
-    kpoints,kx,ky,kz,E,nbands = readBandFile(Filename)
+    kpoints,kx,ky,kz,E,nbands,a,c = readDatFile(Filename)
     print('interpolating bands')
     interp_band_list,bandID = genInterpFuncs(kpoints,kx,ky,kz,E,nbands,a,a,c,E_cutoff,int(N_k))
     
     E_k,dS_k,vz_k,v_k = genEquipotentialGrid(interp_band_list,E_cutoff,N_E,N_theta,N_phi)
     NF_up,NF_down,PN,sigmaz_up0,sigmaz_down0,PNv20,sigmaz_up1,sigmaz_down1,PNv21,sigmaz_up2,sigmaz_down2,PNv22,sigmaz_up5,sigmaz_down5,PNv25,sigmaz_up7,sigmaz_down7,PNv27,sigmaz_up10,sigmaz_down10,PNv210,sigmaz_up15,sigmaz_down15,PNv215,sigmaz_up20,sigmaz_down20,PNv220,sigmaz_up30,sigmaz_down30,PNv230,sigmaz_up40,sigmaz_down40,PNv240,sigmaz_up50,sigmaz_down50,PNv250 = integrateBands(E_k,dS_k,vz_k,v_k,bandID,nbands,T)
-    return [NF_up, NF_down, PN, sigmaz_up0, sigmaz_down0, PNv20,sigmaz_up1,sigmaz_down1,PNv21,sigmaz_up2,sigmaz_down2,PNv22,sigmaz_up5,sigmaz_down5,PNv25,sigmaz_up7,sigmaz_down7,PNv27,sigmaz_up10,sigmaz_down10,PNv210,sigmaz_up15,sigmaz_down15,PNv215,sigmaz_up20,sigmaz_down20,PNv220,sigmaz_up30,sigmaz_down30,PNv230,sigmaz_up40,sigmaz_down40,PNv240,sigmaz_up50,sigmaz_down50,PNv250]
-
-#print(DSP('./qatkDatFiles/atomicMag/Co_bulk_new/a_277.hdf5',2.77,2.77,0.1,8.617e-5*100,15,15,31))
+    return [NF_up, NF_down, PN, sigmaz_up0, sigmaz_down0, PNv20,sigmaz_up1,sigmaz_down1,PNv21,sigmaz_up2,sigmaz_down2,PNv22,sigmaz_up5,sigmaz_down5,PNv25,sigmaz_up7,sigmaz_down7,PNv27,sigmaz_up10,sigmaz_down10,PNv210,sigmaz_up15,sigmaz_down15,PNv215,sigmaz_up20,sigmaz_down20,PNv220,sigmaz_up30,sigmaz_down30,PNv230,sigmaz_up40,sigmaz_down40,PNv240,sigmaz_up50,sigmaz_down50,PNv250,a,c]
 
 def main(EBANDSDirectory,T,nbands,N_theta,N_phi,N_E,E_cut):
     EBANDSFilenameList = []
-    os.system('ls ' + EBANDSDirectory + '*.hdf5 > tmpFileList')
+    os.system('ls ' + EBANDSDirectory + 'a_*.dat > tmpFileList')
     with open('tmpFileList','r') as myFile:
         read = csv.reader(myFile)
         for row in read:
@@ -823,32 +751,14 @@ def main(EBANDSDirectory,T,nbands,N_theta,N_phi,N_E,E_cut):
     os.system('rm tmpFileList')
     
     EBANDSFilenameList = np.array(EBANDSFilenameList)
+    
     a = np.zeros(len(EBANDSFilenameList))
     c = np.zeros(len(EBANDSFilenameList))
-    for i in range(0,len(EBANDSFilenameList)):
-        bulkConfig = nlread(EBANDSFilenameList[i])[0]
-        a[i] = bulkConfig.primitiveVectors()[0][0].inUnitsOf(Angstrom)
-        c[i] = bulkConfig.primitiveVectors()[2][2].inUnitsOf(Angstrom)
-    
-    #a = np.array([2.71,2.72,2.73,2.74,2.75,2.76,2.77,2.78,2.79,2.8,2.81,2.82,2.83,2.84,2.85,2.86,2.87,2.88,2.89,2.90,2.91,2.92,2.93,2.94,2.95])
-    #c = np.array([3.05,3.02,2.99,2.95,2.89,2.83,2.77,2.72,2.68,2.65,2.62,2.6,2.58,2.56,2.54,2.53,2.52,2.51,2.5,2.49,2.48,2.48,2.47,2.46,2.46])
-    
-    #a = np.round(np.arange(2.71,2.96,.01),2)
-    #c = np.array([3.03,3.00,2.96,2.90,2.85,2.80,2.77,2.69,2.67,2.64,2.61,2.59,2.58,2.56,2.55,2.54,2.53,2.51,2.50,2.50,2.49,2.48,2.47,2.46,2.46])
-    
-    idx = np.where((a>2.72))
-    EBANDSFilenameList = EBANDSFilenameList[idx]
-    a = a[idx]
-    c = c[idx]
     
     for i in range(0,len(EBANDSFilenameList)):
-        print(EBANDSFilenameList[i],a[i],c[i])
-    #NOTE: should find a way to extract the a- & c-values from the 
-    # filenames, this won't really matter though since I'm not 
-    # plotting bands
-    #T = 8.617e-5*np.linspace(10,300,30)
+        print(EBANDSFilenameList[i])
+    
     tol = 1e-8
-    #E_cut = T*np.arccosh(1/np.sqrt(4*T*tol))
     
     NF_up = np.zeros(len(EBANDSFilenameList))
     NF_down = np.zeros(len(EBANDSFilenameList))
@@ -888,7 +798,7 @@ def main(EBANDSDirectory,T,nbands,N_theta,N_phi,N_E,E_cut):
     PNv250 = np.zeros(len(EBANDSFilenameList))
     
     for i in range(0,len(EBANDSFilenameList)):
-        results = DSP(EBANDSFilenameList[i],a[i],c[i],E_cut,T,N_theta,N_phi,N_E)
+        results = DSP(EBANDSFilenameList[i],E_cut,T,N_theta,N_phi,N_E)
         
         NF_up[i] = results[0]
         NF_down[i] = results[1]
@@ -926,6 +836,8 @@ def main(EBANDSDirectory,T,nbands,N_theta,N_phi,N_E,E_cut):
         sigmaz_up50[i] = results[33]
         sigmaz_down50[i] = results[34]
         PNv250[i] = results[35]
+        a[i] = results[36]
+        c[i] = results[37]
         print(EBANDSFilenameList[i], a[i], c[i], NF_up[i], NF_down[i], PN[i], sigmaz_up0[i], sigmaz_down0[i], PNv20[i], sigmaz_up1[i], sigmaz_down1[i], PNv21[i], sigmaz_up2[i], sigmaz_down2[i], PNv22[i], sigmaz_up5[i], sigmaz_down5[i], PNv25[i], sigmaz_up7[i], sigmaz_down7[i], PNv27[i], sigmaz_up10[i], sigmaz_down10[i], PNv210[i], sigmaz_up15[i], sigmaz_down15[i], PNv215[i], sigmaz_up20[i], sigmaz_down20[i], PNv220[i], sigmaz_up30[i], sigmaz_down30[i], PNv230[i], sigmaz_up40[i], sigmaz_down40[i], PNv240[i], sigmaz_up50[i], sigmaz_down50[i], PNv250[i])
         f = open('DSP_Co_auto_01.dat','a')
         f.write(str(EBANDSFilenameList[i]) + '\t' + str(a[i]) + '\t' +  str(c[i]) + '\t' + str(NF_up[i]) + '\t' + str(NF_down[i]) + '\t' +  str(PN[i]) + '\t' + str(sigmaz_up0[i]) + '\t' + str(sigmaz_down0[i]) + '\t' + str(PNv20[i]) + '\t' + str(sigmaz_up1[i]) + '\t' + str(sigmaz_down1[i]) + '\t' + str(PNv21[i]) + '\t' + str(sigmaz_up2[i]) + '\t' + str(sigmaz_down2[i]) + '\t' + str(PNv22[i]) + '\t' + str(sigmaz_up5[i]) + '\t' + str(sigmaz_down5[i]) + '\t' + str(PNv25[i]) + '\t' + str(sigmaz_up7[i]) + '\t' + str(sigmaz_down7[i]) + '\t' + str(PNv27[i]) + '\t' + str(sigmaz_up10[i]) + '\t' + str(sigmaz_down10[i]) + '\t' + str(PNv210[i]) + '\t' + str(sigmaz_up15[i]) + '\t' + str(sigmaz_down15[i]) + '\t' + str(PNv215[i]) + '\t' + str(sigmaz_up20[i]) + '\t' + str(sigmaz_down20[i]) + '\t' + str(PNv220[i]) + '\t' + str(sigmaz_up30[i]) + '\t' + str(sigmaz_down30[i]) + '\t' + str(PNv230[i]) + '\t' + str(sigmaz_up40[i]) + '\t' + str(sigmaz_down40[i]) + '\t' + str(PNv240[i]) + '\t' + str(sigmaz_up50[i]) + '\t' + str(sigmaz_down50[i]) + '\t' + str(PNv250[i]) + '\n')
